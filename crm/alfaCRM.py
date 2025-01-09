71232613358b7d162f8daf274f928a468dd99bbe
alex89620109009 <alex89620109009@gmail.com>
2025-01-09 20:38:04 +0300

    The database class and the AlfaCRMDataWrapper class have been rewritten, and a class that writes data to the database has been added

--- crm/alfaCRM.py
+++ crm/alfaCRM.py
@@ -8,32 +8,34 @@ import re
 #Написать декорратор, проверяющий на валидность временный токен
 class AlfaCRM: 
     def __init__(self, hostname:str, email:str, key:str):
-        self.models = {
+        self._models = {
             "Students": "customer/index",
             "Locations": "location/index",
             "Groups": "group/index",
             "Lessons": "lesson/index",
-            "Teachers" : "teacher/index"}
-        self.email = email
-        self.key = key
-        self.hostname = hostname
-        self.header = self._fillHeader()
-        self.brunchId = self._getBrunches()
+            "Teachers" : "teacher/index",
+            "Locations" : "location/index",
+            }
+        self._email = email
+        self._key = key
+        self._hostname = hostname
+        self._fillHeader()
+        self._brunchId = self._getBrunches()
     
         
 
-    def _getTempToken(self):
-        path = f"https://{self.hostname}/v2api/auth/login"
-        r = requests.post(path,json.dumps({'email':self.email, 'api_key':self.key}))
+    def _getTempToken(self) -> str:
+        path = f"https://{self._hostname}/v2api/auth/login"
+        r = requests.post(path,json.dumps({'email':self._email, 'api_key':self._key}))
         return json.loads(r.text)["token"]
 
     
-    def _fillHeader(self):
-        self.header = {'X-ALFACRM-TOKEN': self._getTempToken()}
+    def _fillHeader(self) -> None:
+        self._header = {'X-ALFACRM-TOKEN': self._getTempToken()}
     
-    def _getBrunches(self):    
-        path = f"https://{self.hostname}/v2api/branch/index"
-        r = requests.post(path,data=json.dumps({"is_active" : 1}), headers = self.header)
+    def _getBrunches(self) -> int:    
+        path = f"https://{self._hostname}/v2api/branch/index"
+        r = requests.post(path,data=json.dumps({"is_active" : 1}), headers = self._header)
         brunchDict = json.loads(r.text)
         if "items" in brunchDict:
             return self._getIdBrunches(brunchDict["items"])
@@ -41,7 +43,7 @@ class AlfaCRM:
             self._fillHeader()
             return self._getBrunches()
     
-    def _getIdBrunches(self, brunches:list):       
+    def _getIdBrunches(self, brunches:list) ->int:       
         match(len(brunches)):
             case 1:
                 return brunches[0]["id"]
@@ -49,100 +51,197 @@ class AlfaCRM:
                 return brunches[1]["id"]
 
         
-    def getData(self,model:str, data: dict):
-        path = f"https://{self.hostname}/v2api/{self.brunchId}/{self.models[model]}"
-        r = requests.post(path,data=json.dumps(data),headers = self.header)
+    def getData(self,model:str, data: dict) -> list:
+        path = f"https://{self._hostname}/v2api/{self._brunchId}/{self._models[model]}"
+        r = requests.post(path,data=json.dumps(data),headers = self._header)
         dataDict = json.loads(r.text)
         if "items" in dataDict:
             return dataDict["items"]
         else:
             self._fillHeader()
             return self.getData()
+    
+   
         
 
 #Пересмотреть функции доступа к CRM, из за частых обращений работает крайне долго,
 #Подумать над тем, что бы вытягивать все нужные данные одним запросом.
-class AlfaCRMDataWrapper:
-    def __init__(self,dataBase:DataBase, crm:AlfaCRM, updatePeriotByNexLesson:int = 7, updatePeriodByPrevLesson:int = 7):
-        self.crm = crm
-        self.DB = dataBase
-        self.data = {}
-        self.updatePeriodToNext = updatePeriotByNexLesson
-        self.updatePeriodToPrevious = updatePeriodByPrevLesson
-        self._lastUpdate = None
-       
-   
-    def _fillNextLessons(self) -> list:
+class AlfaCRMDataManager:
+    def __init__(self, crm:AlfaCRM, updatePeriotByNexLesson:int = 7, updatePeriodByPrevLesson:int = 7):
+        self._crm = crm
         
-        dateNextLesson = date.today() + timedelta(self.updatePeriodToNext)
-        nextLessons =[]
-        page = 0
+        self._updatePeriodToNextLesson = 7
+        self._updatePeriodToPreviousLesson = updatePeriodByPrevLesson
+    
+    def _getRegularLessons(self, allLessons:list) -> list:
+        regularLessons = []
+        for i in allLessons:
+            if i['regular_id'] != None:
+                regularLessons.append(i)
+        return regularLessons
+        
+    def _getNextLessonsByLocation(self, locationId:int) ->list:
+        page = 0 
+        dateNextLesson = date.today() + timedelta(self._updatePeriodToNextLesson)
+        data = {'status': 1,'date_from':date.today().strftime('%y-%m-%d'), 'date_to': dateNextLesson.strftime('%y-%m-%d'),'page':page,'location_ids': [locationId]}
+        lessons = []
         while True:
-            temp = self.crm.getData("Lessons", {'status': 1,'date_from':date.today().strftime('%y-%m-%d'), 'date_to':dateNextLesson.strftime('%y-%m-%d'),'page':page})
+            data['page'] = page
+            temp = self._crm.getData("Lessons", data)
             if not temp:
                 break
-            lessonFilter =[]
-            for i in temp:
-                if i['regular_id'] != None:
-                    lessonFilter.append(i)
-            page +=1
-            nextLessons.append(lessonFilter)
-        return nextLessons
+            page+=1
+            lessons.append(self._getRegularLessons(temp))
+        return lessons
     
+    def _getPreviusLessonByGroupId(self, groupId:int) -> list:
+        datePreviousLesson= date.today() - timedelta(self._updatePeriodToPreviousLesson)
+        data = {'status': 3,'group_id':groupId, 'date_from':datePreviousLesson.strftime('%y-%m-%d'), 'date_to':date.today().strftime('%y-%m-%d')}
+        temp = self._crm.getData("Lessons", data)
+        return temp
     
-    def _fillPreviusLessons(self) -> list:
-        datePreviousLesson= date.today() - timedelta(self.updatePeriodToPrevious)
-        previousLesson = []
+    def getLocations(self) -> list:
+        locations = self._crm.getData('Locations', {'is_active':1}) 
+        return self._formatLocationsData(locations)
+   
+    
+    def _formatLocationsData(self, locations:list) ->list:
+        locationsList = []
+        for i in locations:
+            locationsList.append({'id':i['id'], 'name':i['name']})
+        return locationsList
+    
+    def getRegularLessonsByLocationId(self, locationId:int) -> list:
+
+        nextLesson = self._getNextLessonsByLocation(locationId)
+        regularLesson = []
+        for page in nextLesson:
+            for item in page:
+                print(f"getRegularLessonsByLocationId {item}")
+                groupId =item['group_ids'][0]
+                prev = self._getPreviusLessonByGroupId(groupId)[0]
+                regularLesson.append(
+                    {
+                        'idGroup' : groupId,
+                        'topic' : prev['topic'],
+                        'idsStudents': str(item['customer_ids']),
+                        'location': locationId,
+                        'teacher' : item['teacher_ids'][0],
+                        'day' :datetime.strptime(item['date'],'%Y-%m-%d').weekday(),
+                        'timeFrom' : datetime.strptime(item['time_from'],'%Y-%m-%d %H:%M:%S').time().strftime('%H:%M'),
+                        'timeTo' : datetime.strptime(item['time_to'],'%Y-%m-%d %H:%M:%S').time().strftime('%H:%M'),
+                        'maxStudents' : self._getGroupById(groupId)[0]['limit'],
+                        'lastUpdate' : date.today().strftime('Y-%m-%d')
+                    }
+                )
+        return regularLesson
+
+    def _getGroupById(self, groupId:int)->list:
+        return self._crm.getData("Groups", {"id":groupId})
+    
+    def getTeachers(self) -> list:
         page = 0
+        teachers = []
         while True:
-            temp = self.crm.getData("Lessons", {'status': 3, 'date_from':datePreviousLesson.strftime('%y-%m-%d'), 'date_to':date.today().strftime('%y-%m-%d'),'page':page})
-            if not temp:
+            l = self._crm.getData('Teachers', {'removed': 1,'page' : page})
+            if not l:
                 break
-            lessonFilter =[]
-            for i in temp:
-                if i['regular_id'] != None:
-                    lessonFilter.append(i)
+            teachers.append(l)
             page +=1
-            previousLesson.append(lessonFilter)
-        return previousLesson
+        return self._formatTeachersData(teachers)
+
+    def _formatTeachersData(sef, data:list):
+        teachers = []
+        for page in data:
+            for item in page:
+                teachers.append({'id':item['id'], 'name': item['name']})
+        return teachers
     
    
-    def _fillGroups(self)->list:
-        groups = []
-        page =0
-        while True:
-            onePage = self.crm.getData("Groups", {"removed":0, "page":page})
-            page +=1
-            if not onePage:
-                break
-            groups.append(onePage)
-        return groups
-    
-  
-    def _fillStudents(self) ->list:
+        
+    def getStudentsMissedLesson(self, groupId:int) -> list:
+        group = self._getPreviusLessonByGroupId(groupId)[0]
+        skipping = []
+        allStudents = self._getStudents()
+        for student in group['details']:
+            if not student['is_attend']:
+                studentData = self._findStudent(allStudents,'id',student['customer_id'] )
+                if studentData:
+                    skipping.append(
+                    {
+                        'idStudent': student['customer_id'],
+                        'date': group['date'],
+                        'topic': group['topic'],
+                        'idGroup' : group['group_ids'][0],
+                        'idLesson' : group['id'],
+                        'teacher' : group['teacher_ids'][0],
+                        'phoneNumber': studentData['phone'][0] ,
+                        'name': studentData['name']          
+                    }
+                    )
+        return skipping
+
+
+    def _getStudents(self) ->list:
         page =0
         students = []
         while True:
-            onePage = self.crm.getData("Students", {"removed":0, "is_study": 1, "page":page, 'withGroups':False})
+            onePage = self._crm.getData("Students", {"removed":0, "is_study": 1, "page":page, 'withGroups':False})
             page +=1
             if not onePage:
                 break
             students.append(onePage)
         return students
     
-    
-    def _findRecord(self,table:list,key:str,value):
+    def _findStudent(self,table:list,key:str,value):
         for page in table:
             for record in page:
                 if record[key] == value:
                     return record
         return []
     
+    # def _fillTableGroupOccupancy(self):
+    #     groupList =[]
+    #     for page in self._nextLessonsData:
+    #         for lesson in page:
+    #             group = self._findRecord(self._groupData,'id',lesson['group_ids'][0])
+    #             prevLesson = self._findRecord(self._previusLessonData,'group_ids',lesson['group_ids'])
+    #             groupList.append({
+    #                 'idsStudents': ','.join(map(str,lesson['customer_ids'])),
+    #                 'topic': prevLesson['topic'],
+    #                 'location':self._getLocationName(group),
+    #                 'teachers': ', '.join(group['teacher_ids']),
+    #                 'day': datetime.strptime(lesson['date'],'%Y-%m-%d').weekday(),
+    #                 'time': datetime.strptime(lesson['time_from'],'%Y-%m-%d %H:%M:%S').time().strftime('%H:%M'),
+    #                 'assignWorkOffs': 1,
+    #                 'idGroup': lesson["group_ids"][0],
+    #                 'countStudents':len(lesson['customer_ids']),
+    #                 'limit': group['limit']
+    #             })
+    #     self._DB.fillTableGroupOccupancy(groupList)
+    
+    # def _getLocations(self):
+    #     locations = self._crm.getData('Locations', {'is_active':1}) 
+    #     return locations
+
+
+
+
+
+    
+   
+   
+  
+    
+    
+    
+    
+    
     def _fillTempLists(self):
-        self._studentsData =self._fillStudents()
-        self._groupData = self._fillGroups()
-        self._nextLessonsData = self._fillNextLessons()
-        self._previusLessonData = self._fillPreviusLessons()
+        
+        self._groupData = self._getGroups()
+        self._nextLessonsData = self._getNextLessons()
+        self._previusLessonData = self._getPreviusLessons()
 
     def _clearTempLists(self):
         self._studentsData.clear() 
@@ -161,28 +260,8 @@ class AlfaCRMDataWrapper:
         self._fillTableStudentAsences()
         self._clearTempLists()
     
+   
 
-    
-    def _fillTableGroupOccupancy(self):
-        groupList =[]
-        for page in self._nextLessonsData:
-            for lesson in page:
-                group = self._findRecord(self._groupData,'id',lesson['group_ids'][0])
-                prevLesson = self._findRecord(self._previusLessonData,'group_ids',lesson['group_ids'])
-                groupList.append({
-                    'idsStudents': ','.join(map(str,lesson['customer_ids'])),
-                    'topic': prevLesson['topic'],
-                    'location':self._getLocationName(group),
-                    'teachers': ', '.join(group['teacher_ids']),
-                    'day': getDayName(datetime.strptime(lesson['date'],'%Y-%m-%d')),
-                    'time': datetime.strptime(lesson['time_from'],'%Y-%m-%d %H:%M:%S').time().strftime('%H:%M'),
-                    'assignWorkOffs': 1,
-                    'idGroup': lesson["group_ids"][0],
-                    'countStudents':len(lesson['customer_ids']),
-                    'limit': group['limit']
-                })
-        self.DB.fillTableGroupOccupancy(groupList)
-    
     def _fillTableStudentAsences(self):
         
         for page in self._previusLessonData:
@@ -192,118 +271,45 @@ class AlfaCRMDataWrapper:
                     if not students['is_attend']:
                         if student := self._getAsence(lesson,students['customer_id']):
                             record.append(student)
-        self.DB.fillTableStudentAbsences(record)
-    
-    # def _fillStudentAsences(self):
-    #     asencesList =[]
-    #     for lesson in self.previousLessons:
-    #         for details in lesson['details']:
-    #             if details['is_attend'] == 0:
-    #                asencesList.append(self._addStudent(lesson, details))
-    #     self.DB.fillTableStudentAbsences(asencesList)
-
-    def _getAsence(self,lesson:dict, idStudent:int):
-        
-        student = self._findRecord(self._studentsData,'id', idStudent)
-        if student:
-            group = self._findRecord(self._groupData, 'id',lesson['group_ids'][0])
-            return {
-                'id' : idStudent,
-                'name' : student['name'],
-                'date' : lesson['date'],
-                'topic' : lesson['topic'],
-                'idGroups' : ', '.join(map(str,lesson['group_ids'])),
-                'idLesson': lesson['id'],
-                'phoneNumber': student['phone'][0].replace('+','').replace('-','').replace('(','').replace(')',''),
-                'teacher':','.join(group['teacher_ids']),
-            
-            
-            
-        }
-    #Костыль, чисто под нашу CRM, думать, как решить лучше
-    def _getLocationName(self, group:list):
-        if 'ЮГ' in group['name']:
-            return 'Юг'
-        else:
-            return 'Центр'
-
-    
-    # def fillDataBase(self):
-    #     self._fillNextLessons()
-    #     self._fillPreviusLessons()
-    #     self._fillGroupOccupancy()
-    #     self._fillStudentAsences()
-    # def _fillGroupOccupancy(self):
-    #     groupList =[]
-    #     for lesson in self.nextLessons:
-    #         groupList.append({
-    #             'idsStudents': str(lesson['customer_ids']),
-    #             'topic': str(self._getTopic(lesson['group_ids'])),
-    #             'location':str(self._getLocationName(lesson['group_ids'])),
-    #             'teachers': str(self._getTeachersName(lesson['group_ids'])),
-    #             'day': str(getDayName(datetime.strptime(lesson['date'],'%Y-%m-%d'))),
-    #             'time': str(datetime.strptime(lesson['time_from'],'%Y-%m-%d %H:%M:%S').time().strftime('%H:%M:%S')),
-    #             'assignWorkOffs': 1,
-    #             'groupId': lesson["group_ids"][0],
-    #             'countStudents':len(lesson['customer_ids']),
-    #             'limit': self._getGroupLimit(lesson['group_ids'])
-    #         })
-    #     self.DB.fillTableGroupOccupancy(groupList)
-    # def _getGroupLimit(self,id):
-    #     groups = self.crm.getData("Group",{'id':id[0]})
-        
-    #     return groups[0]['limit']
-    
-    # def _getStudentPhoneNumber(self, id):
-    #     student = self.crm.getData("Students", {'id': id})
-    #     if len(student):
-    #         phones = student[0]['phone']
-    #         return phones[0]
-    #     else:
-    #         return "None"
-    # def _getStudentName(self,id):
-       
-    #     student = self.crm.getData("Students", {'id': id})
-    #     if len(student):
-    #         return student[0]['name']
-    #     return "None"
-    
-    # def _getTeachersName(self, id:list):
-    #     groups = self.crm.getData("Group",{'id':id[0]})
-    #     return groups[0]['teacher_ids']
-        
-        
-        
-    
-   
-        
-
-    # def _getTopic(self,id:list):
-    #     for i in self.previousLessons:
-    #         if id[0] in i['group_ids']:
-    #             return i['topic']
-
-def getDayName(dateTtime:datetime):
-    match(datetime.weekday(dateTtime)):
-        case 0:
-            return "Понедельник"
-        case 1:
-            return "Вторник"
-        case 2:
-            return "Среда"
-        case 3:
-            return "Четверг"
-        case 4:
-            return "Пятница"
-        case 5: 
-            return "Суббота"
-        case 6:
-            return "Воскресенье"
+        self._DB.fillTableStudentAbsences(record)
 
     
            
             
-          
+class AlfaCRMDBManager():
+
+    def __init__(self,dataBase:DataBase, alfaCRMDataManager:AlfaCRMDataManager):
+        self.db = dataBase
+        self.dataManager = alfaCRMDataManager
+        pass
+    
+    def synchroniseTeachers(self):
+        self.db.synchroniseTeachers(self.dataManager.getTeachers())
+        print('synchroniseTeachers Ok')
+
+    def synchroniseRegularLessons(self):
+        locations = self.db.getAllLocations()
+        allLessons = []
+        for location in locations:
+            print(f'synchroniseRegularLessons location {location}')
+            lessons = self.dataManager.getRegularLessonsByLocationId(location['id'])
+            allLessons+= lessons
+        self.db.synchroniseTableRegularLessons(allLessons)
+        print(f'synchroniseRegularLessons done')
+
+    def insertInStudentAbsences(self):
+        idsGroups = self.db.getRegularLessonsIds()
+        for i in idsGroups:
+            print(" insertInStudentAbsences group id ", i )
+            students = self.dataManager.getStudentsMissedLesson(i)
+            self.db.fillTableStudentAbsences(students)
+
+
+
+
+
+    
+
         
 
         
