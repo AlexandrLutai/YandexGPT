1bce40f78c8091a72b06f41cecf770b57040f74d
alex89620109009 <alex89620109009@gmail.com>
2025-01-11 22:09:32 +0300

    Added private methods of the DataBase class for retrieving data from tables and converting them to a dictionary.

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import autorizationData.authorizationData as autorization
+
 from crm.alfaCRM import AlfaCRM, AlfaCRMDataManager, AlfaCRMDBManager
 from dataBase.localDB import DataBase, getDateNextWeekday
+
+
+
+
 db = DataBase()
 crm = AlfaCRM(autorization.crmhostname, autorization.crmEmail, autorization.crmKey)
-
+# print(crm.getData('RegularLessons', {}))
 crmManager = AlfaCRMDataManager(crm,7,14)
 
+print(db._selectData('StudentAbsences'))
+
+# Связь между lessons и regularLessons устанавливается полем regularId
+ 
 crmToDBManager = AlfaCRMDBManager(db, crmManager)
 
-for i in crmManager.getLocations():
-    db.insertNewLocation(i)
 
-crmToDBManager.synchroniseTeachers()
-crmToDBManager.synchroniseRegularLessons()
-crmToDBManager.insertInStudentAbsences()
-db.addDataInTableGroupOccupancy()
+#Синхронизация бд и CRM
+# for i in crmManager.getLocations():
+#     db.insertNewLocation(i)
+
+# crmToDBManager.synchroniseTeachers()
+# crmToDBManager.synchroniseRegularLessons()
+# crmToDBManager.insertInStudentAbsences()
+# db.addDataInTableGroupOccupancy()
 
 #Выполняется слишком долго, подумать над хранением данных клиентов локально
 #Добавить класс для внесения изменений в AlfaCRM
